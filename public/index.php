bc7ea3e9c9b549e8677e4d72918468cc2613d18f
Hugo Gehl <gehlhugo@gmail.com>
2019-10-23 17:58:46 +0200

    modif suite à des corrections

--- public/index.php
+++ public/index.php
@@ -7,4 +7,4 @@ $products = ['product1', 'product2', 'product3', 'product4', 'product5'];
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader);
 
-echo $twig->render('index.html.twig_', ['products' => $products]);
+echo $twig->render('index.html.twig', ['products' => $products]);
